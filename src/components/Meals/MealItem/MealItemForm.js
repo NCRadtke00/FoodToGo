fec36192c3dcbbb64433693aed5fbb429a57afae
RadtkeN <raddnc@gmail.com>
2022-04-30 02:39:08 -0500

    adding useRefs to project

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import classes from './MealItemForm.module.css'
 import Input from '../../UI/Input'
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+    }
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input ref={amountInputRef} label="Amount" input={{
                 id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',

7fb5020f29179ba35697f7e0741dc08fb351306c
RadtkeN <raddnc@gmail.com>
2022-05-01 02:23:12 -0500

    working on removing items from cart

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -4,6 +4,6 @@ const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
     addItem: (item) => { },
-    removeItem: (item) => { }
+    removeItem: (id) => { }
 });
 export default CartContext;

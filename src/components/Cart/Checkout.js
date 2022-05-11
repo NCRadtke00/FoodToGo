5dcaf64e5654820875f122a1ee0a2475b9a529eb
RadtkeN <raddnc@gmail.com>
2022-05-11 02:05:13 -0500

    made basic un-styled checkout form

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import classes from './Checkout.module.css';
+
+const Checkout = props => {
+    return (
+        <form>
+            <div className={classes.control}>
+                <label htmlFor='name'> Your Name </label>
+                <input type="text" id='name' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='street'> Street </label>
+                <input type="text" id='street' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='city'> City </label>
+                <input type="text" id='city' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='state'> State </label>
+                <input type="text" id='state' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='postal'> Zip Code </label>
+                <input type="text" id='postal' />
+            </div>
+            <button> Confirm </button>
+        </form>
+    )
+}
+
+export default Checkout

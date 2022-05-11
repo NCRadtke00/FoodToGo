e1e35f24a1de41b5a7f4e6bc5510d4776ae7321a
RadtkeN <raddnc@gmail.com>
2022-05-11 02:50:43 -0500

    adding validation to form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useRef, useState } from 'react'
 import classes from './Checkout.module.css';
 
-const Checkout = props => {
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+    const [formInputValidity, setFormInputValidity] = useState({
+        name: true,
+        street: true,
+        city: true,
+        state: true,
+        postalCode: true,
+    });
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const cityInputRef = useRef();
+    const stateInputRef = useRef();
+    const postalCodeInputRef = useRef();
+
     const confirmHandler = (event) => {
         event.preventDefault();
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredState = stateInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredCityIsValid = !isEmpty(enteredCity);
+        const enteredStateIsValid = !isEmpty(enteredState);
+        const enteredPostalCodeisValid = !isEmpty(enteredPostalCode);
+
     }
     return (
         <form onSubmit={confirmHandler}>
